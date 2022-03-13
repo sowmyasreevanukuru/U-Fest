31eb41a2b14fb03cf85608c55b437a3403df4be4
sowmyasreevanukuru <96767583+sowmyasreevanukuru@users.noreply.github.com>
2022-03-13 07:18:55 +0530

    login coordinator

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const {check,validationResult} = require('express-validator/check');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
 // @route   GET api/auth   
 // @desc    Test route
@@ -15,4 +19,59 @@ router.get('/',auth,async (req,res) => {
         res.status(500).send('Server Error');
     }
 });
+
+// @route   POST api/auth
+// @desc    Authenticate user and get token
+// @access  Public
+router.post('/',[
+    check('email','Email is required').isEmail(),
+    check('password','Password is required').exists()
+],
+async (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    
+    const{email,password} = req.body;
+    try{
+
+        let user = await User.findOne({email});
+
+        //check if user exists 
+        if(!user){
+           return res.status(400).json({errors: [{msg:'Invalid Credentials '}]});
+        }
+        
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res
+            .status(400) 
+            .json({errors: [{msg: 'Invalid Credentials'}]});
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            {expiresIn:360000},
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            }
+        ); 
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+   // res.send('Co-ordinator Registered!');
+}); 
+
 module.exports = router;
