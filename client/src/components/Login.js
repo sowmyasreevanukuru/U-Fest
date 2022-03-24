9235810353b5c971e2444120afae1d080c6877d3
sowmyasreevanukuru <96767583+sowmyasreevanukuru@users.noreply.github.com>
2022-03-25 02:16:36 +0530

    login ui

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -12,10 +12,10 @@ const Login = () => {
                     <div class="card-body p-0">
                         <div class="row">
                             <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-5">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Login here!</h1>
                                     </div>
                                     <form class="user">
                                         <div class="form-group">
@@ -37,21 +37,13 @@ const Login = () => {
                                         <a href="index.html" class="btn btn-primary btn-user btn-block">
                                             Login
                                         </a>
-                                        <hr />
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
+                                        <hr/>
                                     </form>
-                                    <hr />
+                                    
                                     <div class="text-center">
                                         <a class="small" href="forgot-password.html">Forgot Password?</a>
                                     </div>
-                                    <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
-                                    </div>
+                                    <br></br><br></br>
                                 </div>
                             </div>
                         </div>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,17 +5,14 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="Home">U-Fest</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="Home">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="Login">Login</a>
+       <li className="nav-item active">
+          <a className="nav-link" href="Login">Register</a>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -28,14 +25,14 @@ const Navbar = () => {
             <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
+        
       </ul>
       <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        
-        <button className="btn btn-outline-success" type="submit">Search</button>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="btn btn-outline-primary" href="Login">Login</a>
+        </li>
+        </ul>
       </form>
     </div>
   </div>
