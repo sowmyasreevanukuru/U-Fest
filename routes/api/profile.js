c6d99b97f949d526a115dbc465d9cdb40a0a91a9
Nkb1999 <bhaktaneshi@gmail.com>
2022-04-15 20:10:28 +0530

    profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,42 +5,22 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 const {check,validationResult} = require('express-validator/check');
 
-// router.get("/all", async (req, res) => {
-//     User.find((err, User) => {
-//       if (err) {
-//         return res.json({ err: err });
-//       } else if (User == null) {
-//         return res.json({ err: "no profile avalible" });
-//       } else {
-//         return res.json({ data: User });
-//       }
-//     });
-//   });
-  
-// // @route   POST api/users
-// // @desc    Register route
-// // @access  Public
-// router.post('/',
-// async (req,res) => {
-//     const errors = validationResult(req);
-//     if(!errors.isEmpty())
-//     {
-//         return res.status(400).json({errors: errors.array()});
-//     }
+// @route   GET api/profile/
+// @desc    Get all profiles
+// @access  Private
+router.get("/", async (req, res)=>{
+    try {
+        const profile = await User.find({email : req.query.email})
+        //const profile = await User.find().select("-password");
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
 
-//     const salt = await bcrypt.genSalt(10);
-//     let user = new User ({
-//         name:req.body.name,
-//         email:req.body.email,
-//         password: await bcrypt.hash(req.body.password,salt),
-//         department:req.body.department
-//     });
-//     //const{name,email,password,department} = req.body;
 
-// }); 
-
-// module.exports = router;
-
+//
 // const app = express();
 // app.listen(3000, () => console.log('listen at 3000'));
 // app.use(express.static('public'));
@@ -80,46 +60,46 @@ const {check,validationResult} = require('express-validator/check');
 
 // @route   POST api/profile
 // @desc    Create or Update user profile
-// @access  Private
-router.post(
-    '/',
-    [
-        auth, 
-        [
+// // @access  Private
+// router.post(
+//     '/',
+//     [
+//         auth, 
+//         [
            
-        ]
-    ],
-    async(req,res) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(400).json({errors: errors.array()});
-        }
-        const {
+//         ]
+//     ],
+//     async(req,res) => {
+//         const errors = validationResult(req);
+//         if(!errors.isEmpty()){
+//             return res.status(400).json({errors: errors.array()});
+//         }
+//         const {
             
-        } = req.body
+//         } = req.body
 
-        //build profile object
-        const profileFields = {};
-        profileFields.user = req.user.id;
-        try{
-            let profile = Profile.findOne({ user: req.user.id});
-            if (profile){
-                profile = await Profile.findOneAndUpdate(
-                    { user: req.user.id},
-                    {$set: profileFields},
-                    { new: true}
-                );
-                return res.json(profile);
-            }
-            //create 
-            profile = new Profile(profileFields);
-            await profile.save();
-            res.json(profile);
-        }catch(err){
-            console.error(err.message)
-            res.status(500).send('Server error');
-        }
-    }
-);
+//         //build profile object
+//         const profileFields = {};
+//         profileFields.user = req.user.id;
+//         try{
+//             let profile = Profile.findOne({ user: req.user.id});
+//             if (profile){
+//                 profile = await Profile.findOneAndUpdate(
+//                     { user: req.user.id},
+//                     {$set: profileFields},
+//                     { new: true}
+//                 );
+//                 return res.json(profile);
+//             }
+//             //create 
+//             profile = new Profile(profileFields);
+//             await profile.save();
+//             res.json(profile);
+//         }catch(err){
+//             console.error(err.message)
+//             res.status(500).send('Server error');
+//         }
+//     }
+// );
 
 module.exports = router;
